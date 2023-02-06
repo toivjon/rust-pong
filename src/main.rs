0979352c6fb55316003268a488cb0efda2d8fabb
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-02-06 07:51:03 +0200

    Added a support to open a new window.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,61 @@
 use windows::s;
+use windows::Win32::Foundation::*;
+use windows::Win32::System::LibraryLoader::GetModuleHandleA;
 use windows::Win32::UI::WindowsAndMessaging::*;
 
 fn main() {
-    unsafe {
-        MessageBoxA(
-            None,
-            s!("Hello world!"),
-            s!("Important Note"),
-            MB_ICONASTERISK | MB_OK,
-        )
-    };
+    let _window = unsafe { create_window() };
+    let mut msg = MSG::default();
+    loop {
+        while unsafe { PeekMessageA(&mut msg, HWND(0), 0, 0, PM_REMOVE).into() } {
+            if msg.message == WM_QUIT {
+                return;
+            }
+            unsafe { DispatchMessageA(&msg) };
+        }
+    }
+}
+
+unsafe fn create_window() -> HWND {
+    // Acquire the module handle of the application.
+    let instance = GetModuleHandleA(None).unwrap();
+    debug_assert!(!instance.is_invalid());
+
+    // Register a window class for the application.
+    let class_name = s!("window");
+    let class_result = RegisterClassA(&WNDCLASSA {
+        hCursor: LoadCursorW(None, IDC_ARROW).unwrap(),
+        hInstance: instance,
+        lpszClassName: class_name,
+        style: CS_HREDRAW | CS_VREDRAW,
+        lpfnWndProc: Some(wndproc),
+        ..Default::default()
+    });
+    debug_assert!(class_result != 0);
+
+    // Build the application window.
+    CreateWindowExA(
+        WINDOW_EX_STYLE::default(),
+        class_name,
+        s!("Pong"),
+        WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        None,
+        None,
+        instance,
+        None,
+    )
+}
+
+unsafe extern "system" fn wndproc(hwnd: HWND, msg: u32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    match msg {
+        WM_DESTROY => {
+            PostQuitMessage(0);
+            LRESULT(0)
+        }
+        _ => DefWindowProcA(hwnd, msg, wparam, lparam),
+    }
 }
