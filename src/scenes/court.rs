d8a4cb0ebc1c52dfd0ec3fc59a40357b9bc2ab70
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2023-02-18 09:56:39 +0200

    Removedd unnecessary suffixes from the scene module files.

--- src/scenes/court.rs
+++ src/scenes/court.rs
@@ -6,7 +6,7 @@ use crate::{
     app::Scene,
     geometry::{Rectangle, Text},
     graphics::Graphics,
-    scenes::end_game_scene::EndGameScene,
+    scenes::end_game::EndGameScene,
 };
 
 /// A constant for the paddle movement velocity.
--- src/scenes/mod.rs
+++ src/scenes/mod.rs
@@ -1,3 +1,3 @@
-pub mod court_scene;
-pub mod end_game_scene;
-pub mod main_menu_scene;
+pub mod court;
+pub mod end_game;
+pub mod main_menu;
